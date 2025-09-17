e67fee1480ba99228e996bb4814a97a4f1f8b40d
vanitha <vanitha@dilligentech.com>
2025-09-17 14:02:48 +0530

    ch

--- service/TaskService.js
+++ service/TaskService.js
@@ -17,7 +17,6 @@ const updateTask = (id, updatedTask) => {
   const index = tasks.findIndex(t => t.id === id);
   if (index !== -1) {
     tasks[index] = { ...tasks[index], ...updatedTask };
-
     return tasks[index];
   }
   return null;
@@ -26,9 +25,7 @@ const updateTask = (id, updatedTask) => {
 const deleteTask = (id) => {
   const index = tasks.findIndex(t => t.id === id);
   if (index !== -1) {
-
     return tasks.splice(index, 1)[0];
-
   }
   return null;
 };
